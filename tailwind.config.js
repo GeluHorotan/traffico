8f0b1f46258bd5b77ac80da559a01071fe11667f
HorotanGelu <horotangelu17@gmail.com>
2022-12-02 19:15:28 +0200

    ci: cleaning auto-generated files, adding fonts and defining color variables

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./*/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#EE4D47',
+        secondary: '#FEF8F5',
+        accent_primary: '#ED4D47',
+        accent_secondary: '#FDEDE8',
+        accent_black: '#402B2B',
+        disclosure: '#FFF7F5',
+        darkBlue: '#0F264C',
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        dmSerif: ['DM Serif Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};

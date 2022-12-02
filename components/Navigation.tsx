47d3f8027b1e3f80a93e1e57032e438d7153bc9a
HorotanGelu <horotangelu17@gmail.com>
2022-12-02 23:08:41 +0200

    feat: adding basic structure of navigation to layout wrapper

---
+++ components/Navigation.tsx
@@ -0,0 +1,33 @@
+type Props = {};
+
+const Navigation = (props: Props) => {
+  return (
+    <nav className="flex items-center justify-between px-44    absolute  w-full  ">
+      <div className="flex w-full items-center justify-start pt-16 gap-4 ">
+        <svg
+          width="47"
+          height="50"
+          viewBox="0 0 47 50"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            opacity="0.4"
+            d="M-1.90735e-06 17L16 17L16 3.24249e-05L-1.90735e-06 17Z"
+            fill="white"
+          />
+          <path d="M47 17H16V50L47 17Z" fill="white" />
+        </svg>
+        <h3 className="text-white">TRAFICO.</h3>
+      </div>
+      <div className="flex items-center  w-full justify-center pt-12 gap-x-24">
+        <p>ABOUT</p>
+        <p>HOW TO</p>
+        <p>FAQS</p>
+        <p>CONTACT US</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
+import Layout from '../components/layouts/Layout';
 import '../globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
